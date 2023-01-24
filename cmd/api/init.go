f0fc15ac36cf4353e5ac4d0bf3a3bdde0aab0a1c
fajarabdillahfn <fajar.abdillahfn@gmail.com>
2023-01-24 11:45:29 +0700

    feat: add kafka initialization and run

--- cmd/api/init.go
+++ cmd/api/init.go
@@ -1,5 +1,16 @@
 package api
 
+import (
+	"log"
+
+	kafkaWallet "github.com/fajarabdillahfn/go-wallet/internal/broker/kafka"
+)
+
 func initialize()  {
+	log.Println("init broker...")
+	kafkaWallet.Init()
 	
+	go kafkaWallet.AboveThresholdProcessor()
+	
+	go kafkaWallet.BalanceProcessor()
 }
